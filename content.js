8c145eefa34934d8939c792a5df50bd06a980450
Nick Parsons <clapynick@gmail.com>
2020-08-05 21:27:26 +1000

    Fixed bug with observer and DOM Node.

--- content.js
+++ content.js
@@ -1,9 +1,10 @@
 const MAX_SPEED = 10; // max 16
+const STEP = 0.25;
 let last_selection = 1;
 
 const saveSelectedItem = function() {
 	last_selection = +this.value;
-}
+};
 
 const observer = new MutationObserver(([e]) => {
 	let select;
@@ -11,17 +12,19 @@ const observer = new MutationObserver(([e]) => {
 		select = document.getElementById("speed-select");
 
 		let options = ``;
-		for(let speed = 2.5; speed <= MAX_SPEED; speed+=0.5) {
+		for(let speed = 2+STEP; speed <= MAX_SPEED; speed+=STEP) {
 			options += `<option value="${speed}">${speed}</option>`;
 		}
-		select.innerHTML += options; // appen built string to the DOM
+		select.innerHTML += options; // append built string to the DOM
 		select.value = last_selection;
 		select.addEventListener('change', saveSelectedItem);
 	}
 
-	if(e.removedNodes.length && select) { // settings dialogue closed & event listener bound
+	if(e.removedNodes.length && select) { // settings dialog closed & event listener bound
 		select.removeEventListener('change', saveSelectedItem);
 	}
 });
 
-observer.observe(document.querySelector('.settings-menu'), {childList: true});
+window.addEventListener('load', () => {
+	observer.observe(document.querySelector('.settings-menu'), {childList: true});
+});
